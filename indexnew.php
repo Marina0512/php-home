d2a793e79216864f4f19a2b88b63324507a844d9
Marina Ponomareva <ponomareva0512@yandex.ru>
2025-04-08 16:38:49 +0500

    DZ2

---
+++ indexnew.php
@@ -0,0 +1,49 @@
+<?php
+//$variable = 3.14;
+// $variable = 3;
+ $variable = 'one';
+// $variable = true;
+// $variable = null;
+// $variable = [];
+
+if (is_bool($variable)) {
+  $type = 'bool';
+} elseif (is_float($variable)) {
+  $type = 'float';    
+} elseif (is_int($variable)) {
+  $type = 'int';
+} elseif (is_string($variable)) {
+  $type = 'string';
+} elseif (is_null($variable)) {
+  $type = 'null';
+} else {
+    $type = 'other';
+}
+
+echo "type is $type";
+
+
+$variable = 3.14;
+
+switch (true) {
+    case is_bool($variable):
+        $type = 'bool';
+        break;
+    case is_float($variable):
+        $type = 'float';
+        break;
+    case is_int($variable):
+        $type = 'int';
+        break;
+    case is_string($variable):
+            $type = 'string';
+        break;
+    case is_null($variable):
+        $type = 'null';
+        break;
+    default:
+        $type = 'other';
+}
+
+echo "type is $type"
+?>
