c6ba24e8d0423441170d9610cb061869957e0716
Marina Ponomareva <ponomareva0512@yandex.ru>
2025-04-14 10:32:50 +0500

    changes

--- dz2.php
+++ dz2.php
@@ -2,18 +2,20 @@
 
 $number1 = trim(fgets(STDIN));
 
-if (!is_numeric($number1))
+if (!is_numeric($number1)) {
 fwrite(STDERR, "Введите, пожалуйста, число" . PHP_EOL);
-
+exit(1);
+}
 $number2 = trim(fgets(STDIN));
 
-if (!is_numeric($number1))
+if (!is_numeric($number2)) {
 fwrite(STDERR, "Введите, пожалуйста, число" . PHP_EOL);
-
-else ($number2 === 0);
+exit(1);
+}
+if ($number2 == 0);
 fwrite(STDERR, "Делить на 0 нельзя" . PHP_EOL);
 
 $number = $number1 / $number2;
 
-echo(STDOUT. $number . PHP_EOL);
+echo ($number . PHP_EOL);
 ?>
