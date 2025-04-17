58f721e047771796798ea14f63a318b92e2d9341
Marina Ponomareva <ponomareva0512@yandex.ru>
2025-04-17 10:28:43 +0500

    fio changes

--- fio.php
+++ fio.php
@@ -16,9 +16,22 @@ $surname = trim(fgets(STDIN));
 fwrite(STDOUT, "Введите отчество: ");
 $patronymic = trim(fgets(STDIN));
 
-
 $fullname = capitalizeName($surname) . " " . capitalizeName($name) . " " . capitalizeName($patronymic);
 
 echo $fullname . PHP_EOL;
+
+
+$capitalizedName = capitalizeName($name);
+$capitalizedSurname = capitalizeName($surname);
+$capitalizedPatronymic = capitalizeName($patronymic);
+
+$nameInitial = mb_substr($capitalizedName, 0, 1);
+$patronymicInitial = mb_substr($capitalizedPatronymic, 0, 1);
+
+$surnameAndInitials = $capitalizedSurname . " " . $nameInitial . "." . $patronymicInitial . ".";
+$fio = $nameInitial . $surname . $patronymicInitial;
+
+echo "Фамилия и инициалы: " . $surnameAndInitials . PHP_EOL;
+echo "ФИО: " . $fio . PHP_EOL;
 
 ?>
