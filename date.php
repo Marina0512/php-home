9d6962bc9b283b4ca1d5ab5e36a3d6477487125a
Marina Ponomareva <ponomareva0512@yandex.ru>
2025-05-03 13:35:51 +0500

    phpdate

--- date.php
+++ date.php
@@ -12,53 +12,73 @@ function workSchedule(int $year, int $month): void
     $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
     $monthName = date("F", mktime(0, 0, 0, $month, 1, $year));
 
-    echo "$monthName $year:\n"; 
-    
-    $isWorkingDay = true;
-    $dayOffCounter = 0;
+    echo "$monthName $year:\n";
 
-    for ($day = 1; $day <= $daysInMonth; $day++) {
+    $firstDayTimestamp = mktime(0, 0, 0, $month, 1, $year);
+    $firstDayOfWeek = (int)date("N", $firstDayTimestamp);
+
+    for ($i = 1; $i < $firstDayOfWeek; $i++) {
+        echo "    ";
+    }
+
+    $currentDayOfWeek = $firstDayOfWeek;
+    $day = 1;
+    $workDayCounter = 0;
+
+    while ($day <= $daysInMonth) {
         $timestamp = mktime(0, 0, 0, $month, $day, $year);
-        $dayOfWeek = date("N", $timestamp);
+        $dayOfWeek = (int)date("N", $timestamp);
 
-        if ($isWorkingDay) {
-            if ($dayOfWeek >= 6) {
-                $daysUntilMonday = 8 - $dayOfWeek;
-                $newTimestamp = mktime(0, 0, 0, $month, $day + $daysUntilMonday, $year);
+        if ($workDayCounter == 0) {
+        
+            if ($dayOfWeek == 6 || $dayOfWeek == 7) {
+                $daysToMonday = ($dayOfWeek == 6) ? 2 : 1;
+                $monday = $day + $daysToMonday;
 
-                if (date('m', $newTimestamp) != $month) {
-                    printf("%2d(+) ", $day); 
+                if ($monday <= $daysInMonth) {
+                    printf("\033[32m%2d \033[0m ", $monday);
+                    $day = $monday;
+
                 } else {
-                    printf("\033[32m%2d(+)\033[0m ", $day + $daysUntilMonday);
-                }
-            } else {
-                printf("\033[32m%2d(+)\033[0m ", $day);
+                    printf("%2d  ", $day);
+                    }
             }
-            $isWorkingDay = false;
-            $dayOffCounter = 0;
+            else {
+              printf("\033[32m%2d \033[0m ", $day);
+            }
+
+            $workDayCounter = 1;
+
+
         } else {
-            printf("%2d   ", $day);
-            $dayOffCounter++;
-            if ($dayOffCounter == 2) {
-                $isWorkingDay = true;
-            }
+            printf("%2d  ", $day);
+             $workDayCounter++;
+             if ($workDayCounter == 3)
+                $workDayCounter = 0;
+
         }
-        if ($day % 7 === 0) {
-            echo "\n"; 
+
+        $currentDayOfWeek++;
+        if ($currentDayOfWeek > 7) {
+            $currentDayOfWeek = 1;
+            echo "\n";
         }
+
+        $day++;
     }
     echo "\n";
 }
 
-$startYear = (int)date('Y');
-$startMonth = (int)date('m');
+$startYear = 2025;
+$startMonth = 1;
 $numMonths = 1;
+
 
 for ($i = 0; $i < $numMonths; $i++) {
     $year = $startYear;
     $month = $startMonth + $i;
 
-  
+    
     if ($month > 12) {
         $year += floor($month / 12);
         $month = $month % 12;
